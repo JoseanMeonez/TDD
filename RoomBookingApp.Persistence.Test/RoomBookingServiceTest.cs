e3b6c764c471e02e7b0fe8fce06c52e177eae0af
Andrés Meóñez <joseanmeonez@gmail.com>
2024-05-30 17:01:42 -0600

    fix: test "ShouldReturnAvailableRooms"

--- RoomBookingApp.Persistence.Test/RoomBookingServiceTest.cs
+++ RoomBookingApp.Persistence.Test/RoomBookingServiceTest.cs
@@ -22,8 +22,8 @@ public class RoomBookingServiceTest
 		context.Add(new Room { Id = 2, Name = "Room 2" });
 		context.Add(new Room { Id = 3, Name = "Room 3" });
 
-		context.Add(new RoomBooking { Id = 1, Date = date });
-		context.Add(new RoomBooking { Id = 2, Date = date.AddDays(-1) });
+		context.Add(new RoomBooking { RoomId = 1, Date = date });
+		context.Add(new RoomBooking { RoomId = 2, Date = date.AddDays(-1) });
 
 		context.SaveChanges();
 
--- RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
+++ RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
@@ -4,16 +4,20 @@ using RoomBookingApp.Domain;
 namespace RoomBookingApp.Persistence.Repositories;
 public class RoomBookingService : IRoomBookingService
 {
-	private readonly RoomBookingAppDbContext _dbContext;
+	private readonly RoomBookingAppDbContext _context;
 
-	public RoomBookingService(RoomBookingAppDbContext dbContext)
+	public RoomBookingService(RoomBookingAppDbContext context)
 	{
-		_dbContext = dbContext;
+		_context = context;
 	}
 
 	public IEnumerable<Room> GetAvailableRooms(DateTime date)
 	{
-		throw new NotImplementedException();
+		var unAvailableRooms = _context.RoomBookings.Where(rb => rb.Date == date).Select(rb => rb.RoomId).ToList();
+
+		var availableRooms = _context.Rooms.Where(r => unAvailableRooms.Contains(r.Id) == false).ToList();
+
+		return availableRooms;
 	}
 
 	public void Save(RoomBooking roomBooking)
