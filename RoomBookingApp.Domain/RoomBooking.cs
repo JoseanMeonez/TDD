7363718b0eae9900105f52073cb2947fabb0e77d
Andrés Meóñez <joseanmeonez@gmail.com>
2024-05-31 16:16:52 -0600

    fix: remove virtual

--- RoomBookingApp.Domain/RoomBooking.cs
+++ RoomBookingApp.Domain/RoomBooking.cs
@@ -6,5 +6,5 @@ public class RoomBooking : RoomBookingBase
 {
 	public int Id { get; set; }
 	public int RoomId { get; set; }
-	public virtual Room? Room { get; set; }
+	public Room? Room { get; set; }
 }
